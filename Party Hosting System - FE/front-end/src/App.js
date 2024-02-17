571d0a727e98b9f0343813344823665d50ee487c
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-02-17 23:49:04 +0700

    [DuyAn] [Hosting-Server] : add hosting server to firebase

--- Party Hosting System - FE/front-end/src/App.js
+++ Party Hosting System - FE/front-end/src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-9c9XNVKixcE7VqGtr76sCoUa7r4sL3E",
+  authDomain: "swd392-4f72a.firebaseapp.com",
+  projectId: "swd392-4f72a",
+  storageBucket: "swd392-4f72a.appspot.com",
+  messagingSenderId: "403408251192",
+  appId: "1:403408251192:web:43309fd65d10f4ba8cf9cd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 function App() {
   return <Layout />;
 }
