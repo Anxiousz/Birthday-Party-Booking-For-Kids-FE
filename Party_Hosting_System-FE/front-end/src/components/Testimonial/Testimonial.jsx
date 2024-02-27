b24188ce5cb830b484515274d929cf13ba1d7df2
NLN2k2 <107394504+NLN2k2@users.noreply.github.com>
2024-02-27 17:18:23 +0700

    3rd commit

--- Party_Hosting_System-FE/front-end/src/components/Testimonial/Testimonial.jsx
+++ Party_Hosting_System-FE/front-end/src/components/Testimonial/Testimonial.jsx
@@ -38,25 +38,25 @@ const Testimonial = () => {
     <Slider {...settings}>
       <div className="testimonial py-4 px-3">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          labore officia commodi architecto. Itaque, quasi. Fuga, distinctio a?
-          Soluta eos aliquam odit quibusdam non mollitia fugit quisquam rerum
-          facere nihil.
+        We held a birthday party at the restaurant and our experience was wonderful!
+        The staff were very helpful and attentive, they went out of their way to ensure that the party ran smoothly.
+        Share and have fun. The food is delicious and beautifully decorated, creating a delicious and interesting party space.
+        Will definitely come back and recommend to friends!
         </p>
         <div className="d-flex align-items-center gap-4 mt-3">
           <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
           <div>
-            <h6 className="mb-0 mt-3">John Doe</h6>
+            <h6 className="mb-0 mt-3">Phương Thảo</h6>
             <p>Customer</p>
           </div>
         </div>
       </div>
       <div className="testimonial py-4 px-3">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          labore officia commodi architecto. Itaque, quasi. Fuga, distinctio a?
-          Soluta eos aliquam odit quibusdam non mollitia fugit quisquam rerum
-          facere nihil.
+        The service at the restaurant for the birthday party was excellent.
+        The staff were friendly and supportive, they went out of their way to ensure that everything ran smoothly.
+        The food was delicious and varied. , meeting the preferences of all guests.
+        We are very satisfied with this experience!
         </p>
         <div className="d-flex align-items-center gap-4 mt-3">
           <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
@@ -68,10 +68,10 @@ const Testimonial = () => {
       </div>
       <div className="testimonial py-4 px-3">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          labore officia commodi architecto. Itaque, quasi. Fuga, distinctio a?
-          Soluta eos aliquam odit quibusdam non mollitia fugit quisquam rerum
-          facere nihil.
+        We held a birthday party at the restaurant and were really impressed by their professional service and quality.
+        Everything, from the menu to the decoration, was carefully prepared. and beautiful.
+        A commendable point is that the staff was flexible and willing to change the plan according to our requests.
+        We couldn't be more satisfied!
         </p>
         <div className="d-flex align-items-center gap-4 mt-3">
           <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
@@ -83,16 +83,16 @@ const Testimonial = () => {
       </div>
       <div className="testimonial py-4 px-3">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          labore officia commodi architecto. Itaque, quasi. Fuga, distinctio a?
-          Soluta eos aliquam odit quibusdam non mollitia fugit quisquam rerum
-          facere nihil.
+        The birthday party at this restaurant was truly a memorable experience!
+        Delicious food, professional service, and romantic decoration.
+        We had a wonderful night.
+        Thank you to the team." created such a special party.
         </p>
         <div className="d-flex align-items-center gap-4 mt-3">
           <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
           <div>
-            <h6 className="mb-0 mt-3">Lia KKK</h6>
-            <p>Customer</p>
+            <h6 className="mb-0 mt-3">Mạnh Huy</h6>
+            <p>Membership</p>
           </div>
         </div>
       </div>
--- Party_Hosting_System-FE/front-end/src/pages/Register.jsx
+++ Party_Hosting_System-FE/front-end/src/pages/Register.jsx
@@ -13,6 +13,41 @@ const Register = () => {
     password: undefined,
   });
 
+  //đang thử nghiệm form đăng kí thu thập thông tin
+  const [values, setValues] = useState({
+    fullName: "",
+    phoneNumber: "",
+    address: "",
+    email: "",
+  });
+  const handleInputChange = (event) => {
+    /* event.persist(); NO LONGER USED IN v.17*/
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    setValues((values) => ({
+      ...values,
+      [name]: value,
+    }));
+  };
+
+  const [submitted, setSubmitted] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      values.fullName &&
+      values.phoneNumber &&
+      values.email &&
+      values.address
+    ) {
+      setValid(true);
+    }
+    setSubmitted(true);
+  };
+  //đang thử nghiệm form đăng kí thu thập thông tin
+
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
@@ -74,6 +109,99 @@ const Register = () => {
                   Already have an account? <Link to="/login">Login</Link>
                 </p>
               </div>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col /*form đăng kí */>
+            <div className="form-container">
+              <form
+                className="register-form"
+                onSubmit={
+                  handleSubmit
+                } /* bảng hiện thông báo đăng kí thành công, hiện tại chưa có nối và chưa pop-up*/
+              >
+                {submitted && valid && (
+                  <div className="success-message">
+                    <h3>
+                      {" "}
+                      Welcome {values.fullName} {values.phoneNumber}{" "}
+                    </h3>
+                    <div> Your registration was successful! </div>
+                  </div>
+                )}
+
+                {!valid && (
+                  <input
+                    class="form-field"
+                    type="text"
+                    placeholder="Full Name"
+                    name="fullName"
+                    value={values.fullName}
+                    onChange={handleInputChange}
+                  />
+                )}
+
+                {submitted && !values.fullName && (
+                  <span id="full-name-error">Please enter your full name</span>
+                )}
+
+                {!valid && (
+                  <input
+                    class="form-field"
+                    type="text"
+                    placeholder="Phone Number"
+                    name="phoneNumber"
+                    value={values.phoneNumber}
+                    onChange={handleInputChange}
+                  />
+                )}
+
+                {submitted && !values.phoneNumber && (
+                  <span id="last-name-error">
+                    Please enter your phone number
+                  </span>
+                )}
+
+                {!valid && (
+                  <input
+                    class="form-field"
+                    type="text"
+                    placeholder="Address"
+                    name="address"
+                    value={values.address}
+                    onChange={handleInputChange}
+                  />
+                )}
+
+                {submitted && !values.address && (
+                  <span id="address-error">Please enter your address</span>
+                )}
+
+                {!valid && (
+                  <input
+                    class="form-field"
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleInputChange}
+                  />
+                )}
+
+                {submitted && !values.email && (
+                  <span id="email-error">Please enter an email address</span>
+                )}
+                {!valid && (
+                  <button class="form-field" type="submit">
+                    Register
+                  </button>
+                )}
+                <p className="change-page">
+                  Already have an account? <Link to="/login">Login</Link>
+                </p>
+              </form>
             </div>
           </Col>
         </Row>
